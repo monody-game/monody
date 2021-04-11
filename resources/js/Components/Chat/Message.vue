4fea77aec34c69619d2f23c9f193abacb3bac1fc
moon250 <mooneupho@gmail.com>
2021-04-11 19:45:41 +0200

    feat: add messages's style and finish chat css

--- resources/js/Components/Chat/Message.vue
+++ resources/js/Components/Chat/Message.vue
@@ -1,12 +1,22 @@
 <template>
-    
+    <div class="message__main">
+        <img :src="json_message.author.avatar" alt="" class="message__avatar">
+        <div class="message__texts">
+            <p class="message__author">{{ json_message.author.username }}</p>
+            <p class="message__content">{{ json_message.content }}</p>
+        </div>
+    </div>
 </template>
 
 <script>
 export default {
-    setup() {
-        
-    },
+    "name": "Message",
+    props: ['message'],
+    data: function () {
+        return {
+            json_message: this.message
+        }
+    }
 }
 </script>
 
