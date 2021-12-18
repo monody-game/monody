a96a90461e279a3427365fc7e86d716b0189a35b
moon250 <mooneupho@gmail.com>
2021-12-18 20:26:17 +0100

    feat: add redis game storage

--- app/Http/Controllers/Api/GameController.php
+++ app/Http/Controllers/Api/GameController.php
@@ -3,31 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
-use App\Models\Game;
 use Firebase\JWT\JWT;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Redis;
 use Illuminate\Support\Facades\Validator;
 
 class GameController extends Controller
 {
-    public function users(Request $request, int $id): JsonResponse
+    public function users(Request $request): JsonResponse
     {
-        $users = DB::select(
-            DB::raw(
-                <<<SQL
-    SELECT `users`.`id`, `users`.`username`, `users`.`avatar` FROM `users`
-    INNER JOIN `game_users` ON `game_users`.`game_id` = :game_id
-    WHERE `users`.`id` = `game_users`.`user_id`
-SQL
-            ),
-            [
-                'game_id' => $id
-            ]
-        );
-
-        return response()->json(['users' => $users]);
+        return response()->json(['users' => []]);
     }
 
     public function token(Request $request): JsonResponse
@@ -48,9 +34,7 @@ SQL
 
     public function list(Request $request): JsonResponse
     {
-        $games = Game::limit(25)->get();
-
-        return response()->json(['games' => $games, 'message' => 'Listed successfully']);
+        return response()->json(['games' => Redis::keys('game:*')]);
     }
 
     public function new(Request $request): JsonResponse
@@ -58,20 +42,40 @@ SQL
         $data = $request->all();
 
         $validator = Validator::make($data, [
-            'users' => 'json|required',
+            'users' => 'json',
             'roles' => 'json|required',
             'is_started' => 'boolean|required'
         ]);
 
         if ($validator->fails()) {
-            return response()->json(['error' => $validator->errors(), 'Validation Error'], 400);
+            return response()->json(['error' => $validator->errors()], 400);
         }
 
+        $data['users'] = \array_key_exists('users', $data) ? json_decode($data['users'], true) : [];
         $data['owner_id'] = $request->user()->id;
+        $data['is_started'] = (bool) $data['is_started'];
+        $id = $this->generateGameId();
 
-        $game = Game::create($data);
+        if (!array_search($data['owner_id'], $data['users'], true)) {
+            $data['users'] = array_merge($data['users'], [$data['owner_id']]);
+        }
 
-        return response()->json(['message' => 'Game created !', 'game' => $game]);
+        Redis::set('game:' . $id, json_encode($data));
+
+        return response()->json(['game' => $data]);
+    }
+
+    private function generateGameId(): string
+    {
+        $bytes = random_bytes(10);
+        $id = base64_encode($bytes);
+        $id = str_replace(['+', '/', '='], '', $id);
+
+        if (Redis::exists('game:' . $id)) {
+            return $this->generateGameId();
+        }
+
+        return $id;
     }
 
     public function delete(Request $request): JsonResponse
@@ -79,15 +83,15 @@ SQL
         $data = $request->all();
 
         $validator = Validator::make($data, [
-            'game_id' => 'integer|required|exists:games,id'
+            'game_id' => 'string|required',
         ]);
 
         if ($validator->fails()) {
-            return response()->json(['error' => $validator->errors(), 'Validation Error'], 400);
+            return response()->json(['error' => $validator->errors()], 400);
         }
 
-        Game::destroy($data['game_id']);
+        Redis::del('game:' . $data['game_id']);
 
-        return response()->json(['message' => 'Game successfully deleted']);
+        return response()->json();
     }
 }
