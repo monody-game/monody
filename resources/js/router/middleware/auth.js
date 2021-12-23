01c608e82ca2e18f1b8ad3d194f48ed6256ec782
moon250 <mooneupho@gmail.com>
2021-12-23 19:13:01 +0100

    fix(dev): auth.js middleware

--- resources/js/router/middleware/auth.js
+++ resources/js/router/middleware/auth.js
@@ -1,8 +1,6 @@
 export default function auth({ next, router }) {
   if (!localStorage.getItem('access-token')) {
     return router.push({ name: 'login' });
-  } else if (!sessionStorage.getItem('access-token')) {
-    return router.push({ name: 'login' });
   }
 
   return next();
