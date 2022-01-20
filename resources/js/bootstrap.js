a11baee33825a05a6be7a1193d37adc13b123515
moon250 <mooneupho@gmail.com>
2022-01-20 19:12:19 +0100

    fix(dev): cors strange bug

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -5,5 +5,6 @@ window.io = require('socket.io-client')
 window.Echo = new Echo({
   broadcaster: 'socket.io',
   host: window.location.hostname + ':6001',
+  transports: ['websocket', 'polling', 'flashsocket'],
   auth: {headers: {Authorization: "Bearer " + localStorage.getItem('access-token')}}
 });
