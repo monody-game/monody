eb987d39659010bfd61ccb8bc302a7cecd3fe665
moon250 <mooneupho@gmail.com>
2022-03-30 14:45:26 +0200

    tests(dev): add AvatarGenerator tests

--- app/Exceptions/FileExtensionException.php
+++ app/Exceptions/FileExtensionException.php
@@ -7,8 +7,8 @@ use Throwable;
 
 class FileExtensionException extends Exception
 {
-    public function __construct(string $extension = '', int $code = 415, ?Throwable $previous = null)
+    public function __construct(string $extension = "", int $code = 415, ?Throwable $previous = null)
     {
-        parent::__construct("Unsupported file extension : ${$extension}", $code, $previous);
+        parent::__construct("Unsupported file extension : $extension", $code, $previous);
     }
 }
