da05f6bc0894ab7fc6e1503a51f4cbc0ab81e31e
moon250 <mooneupho@gmail.com>
2022-04-10 01:19:43 +0200

    fix: ci

--- app/Http/Middleware/AddTokenToRequests.php
+++ app/Http/Middleware/AddTokenToRequests.php
@@ -7,10 +7,16 @@ use Illuminate\Http\Request;
 
 class AddTokenToRequests
 {
+    /**
+     * Attach secure cookie stored access token to the request.
+     *
+     * @return mixed
+     */
     public function handle(Request $request, Closure $next)
     {
         if (!$request->bearerToken()) {
             if ($request->hasCookie('monody_access_token')) {
+                /** @var string $token */
                 $token = $request->cookie('monody_access_token');
 
                 $request->headers->add([
