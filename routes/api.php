d6bba2263d8ea3a18c8d00db2e0fcbbebb8d8afd
moon250 <mooneupho@gmail.com>
2022-05-31 16:21:54 +0200

    feat: prefixed controllers namespace with Api

--- routes/api.php
+++ routes/api.php
@@ -2,32 +2,35 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::post('/auth/login', 'Api\AuthController@login');
-Route::post('/auth/register', 'Api\AuthController@register');
-Route::get('/oauth/discord/link', 'Api\OauthController@discord');
-Route::get('/oauth/google/link', 'Api\OauthController@google');
+Route::post('/auth/login', 'Auth\LoginController@login');
+Route::post('/auth/register', 'Auth\RegisterController@register');
+
+Route::get('/oauth/discord/link', 'OauthController@discordLink');
+Route::get('/oauth/google/link', 'OauthController@googleLink');
+Route::get('/oauth/discord/check', 'OauthController@discordCheck');
+Route::get('/oauth/google/check', 'OauthController@googleCheck');
 
 Route::group(['middleware' => ['auth:api']], function () {
-    Route::get('/avatar/{path}', 'Api\AvatarController@show');
-    Route::get('/avatars/generate', 'Api\AvatarController@generate');
+    Route::get('/avatar/{path}', 'AvatarController@show');
+    Route::get('/avatars/generate', 'AvatarController@generate');
 
-    Route::post('/auth/logout', 'Api\AuthController@logout');
+    Route::post('/auth/logout', 'Auth\LoginController@logout');
 
-    Route::get('/user', 'Api\AuthController@user');
-    Route::get('/user/avatar', 'Api\UserController@avatar');
+    Route::get('/user', 'UserController@user');
+    Route::get('/user/avatar', 'UserController@avatar');
 
-    Route::get('/game/list', 'Api\GameController@list');
-    Route::post('/game/new', 'Api\GameController@new');
-    Route::post('/game/delete', 'Api\GameController@delete');
-    Route::post('/game/check', 'Api\GameController@check');
+    Route::get('/game/list', 'GameController@list');
+    Route::post('/game/new', 'GameController@new');
+    Route::post('/game/delete', 'GameController@delete');
+    Route::post('/game/check', 'GameController@check');
 
-    Route::get('/game/users', 'Api\GameUsersController@list');
-    Route::post('/game/message/send', 'Api\GameMessageController@send');
+    Route::get('/game/users', 'GameUsersController@list');
+    Route::post('/game/message/send', 'GameMessageController@send');
 
-    Route::get('/roles', 'Api\RoleController@all');
-    Route::get('/roles/get/{id}', 'Api\RoleController@get');
+    Route::get('/roles', 'RoleController@all');
+    Route::get('/roles/get/{id}', 'RoleController@get');
 
-    Route::get('/teams', 'Api\TeamController@all');
+    Route::get('/teams', 'TeamController@all');
 
-    Route::get('/exp/get', 'Api\ExpController@get');
+    Route::get('/exp/get', 'ExpController@get');
 });
