6e72d2cd674ae5570d32b853fdda1365f6091e91
moon250 <mooneupho@gmail.com>
2022-06-10 23:33:42 +0200

    fix: node-fetch selfsigned certificate rejection

--- websockets/Channels/PrivateChannel.js
+++ websockets/Channels/PrivateChannel.js
@@ -1,4 +1,9 @@
 const fetch = require("node-fetch");
+const https = require("node:https");
+
+const agent = new https.Agent({
+	rejectUnauthorized: false,
+});
 
 module.exports.PrivateChannel = class {
 	authenticate(socket, data) {
@@ -10,7 +15,7 @@ module.exports.PrivateChannel = class {
 		};
 
 		if (process.env.APP_DEBUG) {
-			console.info(`[${new Date().toISOString()}] - Sending auth request to: "http://web"\n`);
+			console.info(`[${new Date().toISOString()}] - Sending auth request to: "https://web"\n`);
 		}
 
 		return this.serverRequest(socket, options);
@@ -25,17 +30,18 @@ module.exports.PrivateChannel = class {
 	async serverRequest(socket, options) {
 		let body;
 		let response;
-		let status;
+		let status = 0;
 
 		try {
 			const params = new URLSearchParams();
 
 			params.set("channel_name", options.form.channel_name);
 
-			response = await fetch("http://web/broadcasting/auth", {
+			response = await fetch("https://web/broadcasting/auth", {
 				method: "POST",
 				headers: this.prepareHeaders(socket, options),
 				body: params,
+				agent
 			});
 
 			status = response.statusCode;
@@ -58,14 +64,14 @@ module.exports.PrivateChannel = class {
 					console.error(`[${new Date().toISOString()}] - Error authenticating ${socket.id} for ${options.form.channel_name}`);
 					console.error(error);
 				}
-				throw ({ reason: "Error sending authentication request.", status: 0 });
+				throw ({ reason: "Error sending authentication request.", status });
 			} else if (status !== 200) {
 				if (process.env.APP_DEBUG) {
 					console.warn(`[${new Date().toISOString()}] - ${socket.id} could not be authenticated to ${options.form.channel_name}`);
 					console.error(response);
 				}
 
-				throw ({ reason: "Client can not be authenticated, got HTTP status " + status, status: status });
+				throw ({ reason: "Client can not be authenticated, got HTTP status " + status, status });
 			}
 		}
 	}
@@ -73,7 +79,6 @@ module.exports.PrivateChannel = class {
 	prepareHeaders(socket, options) {
 		options.headers["Cookie"] = options.headers["Cookie"] || socket.request.headers.cookie;
 		options.headers["X-Requested-With"] = "XMLHttpRequest";
-		// options.headers['Content-Type'] = 'application/x-www-form-urlencoded';
 
 		return options.headers;
 	}
