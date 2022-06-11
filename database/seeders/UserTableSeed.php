25ba927f8c35d205adeecc85dce781157c0515dd
moon250 <mooneupho@gmail.com>
2022-06-11 23:03:06 +0200

    feat: fix avatar path in user seeder

--- database/seeders/UserTableSeed.php
+++ database/seeders/UserTableSeed.php
@@ -18,7 +18,7 @@ class UserTableSeed extends Seeder
         DB::table('users')->insert([
             'username' => 'moon250',
             'email' => 'mooneupho@gmail.com',
-            'avatar' => '/images/avatars/1.png',
+            'avatar' => '/storage/avatars/1.png',
             'level' => 100,
             'password' => bcrypt('moon250bg'),
 			'created_at' => Carbon::now(),
