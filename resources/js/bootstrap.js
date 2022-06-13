5c98f6f03073b1287a4a0975e93255c75c6a3adb
moon250 <mooneupho@gmail.com>
2022-06-13 16:13:23 +0200

    fix: replace console.log with console.info

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -13,15 +13,15 @@ window.Echo = new Echo({
 });
 
 setTimeout(() => {
-	console.log("%cAttention !", "color: #273097; font-size: 64px; font-weight: bold");
-	console.log("%cSi quelqu'un vous a demandé de coller quelque chose ici, il y a à peu près 100 chances sur 10 que ça soit une arnaque.", "font-size: 20px");
-	console.log("%cFermez cette fenêtre sauf si vous savez exactement ce que vous faites.", "color: red; font-size: 20px");
+	console.info("%cAttention !", "color: #273097; font-size: 64px; font-weight: bold");
+	console.info("%cSi quelqu'un vous a demandé de coller quelque chose ici, il y a à peu près 100 chances sur 10 que ça soit une arnaque.", "font-size: 20px");
+	console.info("%cFermez cette fenêtre sauf si vous savez exactement ce que vous faites.", "color: red; font-size: 20px");
 }, 200);
 
 window.addEventListener("devtoolschange", event => {
 	if (event.detail.isOpen) {
-		console.log("%cAttention !", "color: #273097; font-size: 64px; font-weight: bold");
-		console.log("%cSi quelqu'un vous a demandé de coller quelque chose ici, il y a à peu près 100 chances sur 10 que ça soit une arnaque.", "font-size: 20px");
-		console.log("%cFermez cette fenêtre sauf si vous savez exactement ce que vous faites.", "color: red; font-size: 20px");
+		console.info("%cAttention !", "color: #273097; font-size: 64px; font-weight: bold");
+		console.info("%cSi quelqu'un vous a demandé de coller quelque chose ici, il y a à peu près 100 chances sur 10 que ça soit une arnaque.", "font-size: 20px");
+		console.info("%cFermez cette fenêtre sauf si vous savez exactement ce que vous faites.", "color: red; font-size: 20px");
 	}
 });
