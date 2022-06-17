b688eb108adc370c8b334f3ed7396e5770dd9763
moon250 <mooneupho@gmail.com>
2022-06-17 11:46:49 +0200

    feat: user update

--- tests/Unit/Http/Controllers/Api/UserControllerTest.php
+++ tests/Unit/Http/Controllers/Api/UserControllerTest.php
@@ -3,6 +3,7 @@
 namespace Http\Controllers\Api;
 
 use App\Models\User;
+use Symfony\Component\HttpFoundation\Response;
 use Tests\TestCase;
 
 class UserControllerTest extends TestCase
@@ -12,6 +13,25 @@ class UserControllerTest extends TestCase
 			->actingAs($this->user, 'api')
 			->get('/api/user')
 			->assertJson($this->user->toArray());
+	}
+
+	public function testUpdatingUser() {
+		$user = User::factory()->makeOne([
+			'username' => "John",
+			'email' => "testemail@test.com"
+		]);
+
+		$this
+			->actingAs($user, 'api')
+			->patch('/api/user', [
+				'email' => "emailtest@test.com"
+			])
+			->assertStatus(Response::HTTP_NO_CONTENT);
+
+		$user = $user->refresh();
+
+		$this->assertSame("John", $user->username);
+		$this->assertSame("emailtest@test.com", $user->email);
 	}
 
 	protected function setUp(): void
