b38dabcd0d06050caac0c50614fcff727a5a5011
moon250 <mooneupho@gmail.com>
2022-06-21 15:15:55 +0200

    fix: middlewares

--- resources/js/router/middleware/gameExists.js
+++ resources/js/router/middleware/gameExists.js
@@ -1,15 +1,14 @@
-export default async function exists({ next, router, to }) {
+export default async function exists({ router, to }) {
 	if (!to.params.id) {
-		return router.push({ name: "play" });
+		router.push({ name: "play" });
+		return;
 	}
 
 	const response = await window.JSONFetch("/game/check", "POST", {
 		game_id: to.params.id,
 	});
 
-	if (response.data) {
-		return next();
-	} else {
-		return router.push({ name: "play" });
+	if (response.status === "404") {
+		router.push({ name: "play" });
 	}
 }
