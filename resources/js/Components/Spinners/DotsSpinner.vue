5719609b7412d18bf22c1dbc37bafd3e9ec6fb93
moon250 <mooneupho@gmail.com>
2022-06-29 18:42:19 +0200

    feat: change logo spinner with dots spinner for buttons

---
+++ resources/js/Components/Spinners/DotsSpinner.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="spinner__dots-container">
+    <spinning-dots style="width:24px;" />
+  </div>
+</template>
+<script>
+import SpinningDots from "@grafikart/spinning-dots-element";
+
+customElements.define("spinning-dots", SpinningDots);
+
+export default {
+	name: "DotsSpinner"
+};
+</script>
