e0a2b776128fbcd5722fbf69d33a6fbcab902d21
moon250 <mooneupho@gmail.com>
2022-07-16 16:09:48 +0200

    fix: string|null as return type

--- app/Traits/RegisterHelperTrait.php
+++ app/Traits/RegisterHelperTrait.php
@@ -8,13 +8,15 @@ trait RegisterHelperTrait
 {
     public function setCurrentUserGameActivity(string $userId, string $gameId): void
     {
+        /** @var User $user */
         $user = User::find($userId);
         $user->current_game = $gameId;
         $user->save();
     }
 
-    public function getCurrentUserGameActivity(string $userId): string
+    public function getCurrentUserGameActivity(string $userId): ?string
     {
+        /** @var User $user */
         $user = User::find($userId);
 
         return $user->current_game;
