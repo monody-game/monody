8f894a0cbe010dbc226ad72ef1b46e74c8d96226
moon250 <mooneupho@gmail.com>
2022-07-17 17:19:57 +0200

    fix: get value of enum

--- app/Http/Controllers/Api/Game/GameController.php
+++ app/Http/Controllers/Api/Game/GameController.php
@@ -14,6 +14,7 @@ use App\Traits\RegisterHelperTrait;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Facades\Redis;
 use Symfony\Component\HttpFoundation\Response;
+use function array_key_exists;
 
 class GameController extends Controller
 {
@@ -91,11 +92,11 @@ class GameController extends Controller
         /** @var User $user */
         $user = $request->user();
 
-        $data['users'] = \array_key_exists('users', $data) ? $data['users'] : [];
+        $data['users'] = array_key_exists('users', $data) ? $data['users'] : [];
         $data['roles'] = array_count_values($data['roles']);
         $data['assigned_roles'] = [];
         $data['owner'] = $user->id;
-        $data['is_started'] = \array_key_exists('is_started', $data) ? $data['is_started'] : false;
+        $data['is_started'] = array_key_exists('is_started', $data) ? $data['is_started'] : false;
         $id = $this->generateGameId();
 
         if (!array_search($data['owner'], $data['users'], true)) {
@@ -105,7 +106,7 @@ class GameController extends Controller
         }
 
         Redis::set("game:$id", json_encode($data));
-        Redis::set("game:$id:state", GameStates::WAITING_STATE);
+        Redis::set("game:$id:state", GameStates::WAITING_STATE->value);
         Redis::set("game:$id:votes", json_encode([]));
 
         $data['id'] = $id;
