64a96dc07ee8cd3083175d0ee6129c33f0857331
moon250 <mooneupho@gmail.com>
2022-08-19 22:10:16 +0200

    feat: replace static werewolf role list with api call

--- websockets/Services/RoleService.js
+++ websockets/Services/RoleService.js
@@ -1,4 +1,6 @@
-export class RoleManager {
+import fetch from "../Helpers/fetch.js";
+
+export class RoleService {
 	static assign(roles, members) {
 		const assigned = {};
 
@@ -16,11 +18,21 @@ export class RoleManager {
 		return assigned;
 	}
 
+	/**
+	 * @param {Object[]} members
+	 * @param {Object} assigned
+	 * @returns {Object}
+	 */
 	static pickUser(members, assigned) {
 		let member = members[Math.floor(Math.random() * members.length)].user_id;
 		while (assigned.hasOwnProperty(member)) {
 			member = members[Math.floor(Math.random() * members.length)].user_id;
 		}
 		return member;
+	}
+
+	static async getWerewolvesRoles() {
+		const werewolves = await fetch("https://web/api/roles/2", { method: "GET" });
+		return werewolves.json.roles;
 	}
 }
