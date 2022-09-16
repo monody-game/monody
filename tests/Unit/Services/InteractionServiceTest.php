57f1a96a853157f69ff3ff074e97aee477527d47
moon250 <mooneupho@gmail.com>
2022-09-16 21:01:53 +0200

    fix: change rounds and states to correspond with background and theme

--- tests/Unit/Services/InteractionServiceTest.php
+++ tests/Unit/Services/InteractionServiceTest.php
@@ -5,13 +5,10 @@ namespace Tests\Unit\Services;
 use App\Enums\InteractionActions;
 use App\Enums\Interactions;
 use App\Enums\Roles;
-use App\Events\InteractionClose;
-use App\Events\InteractionCreate;
 use App\Facades\Redis;
 use App\Models\User;
 use App\Services\InteractionService;
 use App\Traits\MemberHelperTrait;
-use Illuminate\Support\Facades\Event;
 use Tests\TestCase;
 
 class InteractionServiceTest extends TestCase
@@ -32,8 +29,6 @@ class InteractionServiceTest extends TestCase
 
     public function testCreatingAnInteraction()
     {
-        Event::fake();
-
         $expectedInteraction = [
             'gameId' => 'test',
             'authorizedCallers' => '*',
@@ -41,17 +36,6 @@ class InteractionServiceTest extends TestCase
         ];
 
         $interaction = $this->service->create('test', Interactions::Vote);
-
-        Event::assertDispatched(function (InteractionCreate $event) use ($interaction) {
-            $event = (array) $event;
-
-            return $event['payload'] === [
-                'gameId' => 'test',
-                'interactionId' => $interaction['interactionId'],
-                'authorizedCallers' => '*',
-                'type' => Interactions::Vote->value,
-            ];
-        });
 
         $redisInteraction = Redis::get('game:test:interactions');
         $expectedInteraction['interactionId'] = $interaction['interactionId'];
@@ -62,19 +46,10 @@ class InteractionServiceTest extends TestCase
 
     public function testEndingInteraction()
     {
-        Event::fake();
-
         $interaction = $this->service->create('otherGame', Interactions::Vote);
-        $this->service->close('otherGame', $interaction['interactionId']);
+        $this->assertNull($this->service->close('otherGame', $interaction['interactionId']));
 
         $this->assertEmpty(Redis::get('game:otherGame:interactions'));
-
-        Event::assertDispatched(function (InteractionClose $event) use ($interaction) {
-            return $event->payload === [
-                'gameId' => 'otherGame',
-                'interactionId' => $interaction['interactionId'],
-            ];
-        });
     }
 
     public function testEndingUnexistantInteraction()
