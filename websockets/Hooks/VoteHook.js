28ce9b0d395f0c0065624c2d7b3a8d5ae7eefc5b
moon250 <mooneupho@gmail.com>
2022-11-12 20:59:16 +0100

    fix: interaction.(open|close) event emitting

--- websockets/Hooks/VoteHook.js
+++ websockets/Hooks/VoteHook.js
@@ -3,21 +3,19 @@ import { client } from "../Redis/Connection.js";
 
 export default {
 	identifier: 7,
-	async before(io, channel, members) {
+	async before(io, channel) {
 		const gameId = channel.split(".")[1];
 		const params = new URLSearchParams();
 		params.set("gameId", gameId);
 		params.set("type", "vote");
 
-		let interactionId = await fetch("https://web/api/interactions", { method: "POST", body: params });
-		interactionId = interactionId.json.interaction.id;
+		const interaction = await fetch("https://web/api/interactions", { method: "POST", body: params });
+		const interactionId = interaction.json.interaction.id;
 		console.log(`Created vote interaction with id ${interactionId} in game ${gameId}`);
 
-		members.forEach(member => {
-			io.to(member.socketId).emit("interaction.open", { interactionId }, channel);
-		});
+		io.to(channel).emit("interaction.open", channel, { interaction: interaction.json.interaction });
 	},
-	async after(io, channel, members) {
+	async after(io, channel) {
 		const gameId = channel.split(".")[1];
 		let interactions = JSON.parse(await client.get(`game:${gameId}:interactions`));
 
@@ -29,8 +27,6 @@ export default {
 		console.log(`Closing vote interaction with id ${interactions.id} in game ${gameId}`);
 		await fetch("https://web/api/interactions", { method: "DELETE", body: params });
 
-		members.forEach(member => {
-			io.to(member.socketId).emit("interaction.close", { interactionId: interactions.id }, channel);
-		});
+		io.to(channel).emit("interaction.close", channel, { interaction: interactions });
 	},
 };
