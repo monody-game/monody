acb636ae454e4bdeffd6c3adf5ef19f37fb383f8
moon250 <mooneupho@gmail.com>
2022-11-23 20:09:39 +0100

    feat: front vote (#19)

--- resources/js/stores/game.js
+++ resources/js/stores/game.js
@@ -1,14 +1,11 @@
 import { defineStore } from "pinia";
-import { useStore as useUserStore } from "./user";
-
-const WEREWOLF_GROUP = 2;
 
 export const useStore = defineStore("game", {
 	state: () => {
 		return {
 			playerList: [],
-			currentVote: 0,
-			state: 0
+			state: 0,
+			currentInteractionId: ""
 		};
 	},
 	actions: {
@@ -24,26 +21,6 @@ export const useStore = defineStore("game", {
 				this.playerList[index] = player;
 			}
 		},
-		setVote({ userID, votedBy }) {
-			this.getPlayerByID(userID).voted_by.push(votedBy);
-			this.currentVote = userID;
-		},
-		getVotes(userID) {
-			return this.getPlayerByID(userID).voted_by;
-		},
-		unVote({ userID, votedBy }) {
-			if (userID === 0) {
-				return;
-			}
-			this.currentVote = 0;
-			const votes = this.getPlayerByID(userID).voted_by;
-			if (votes.length === 1) {
-				this.getPlayerByID(userID).voted_by = [];
-				return;
-			}
-			const index = votes.indexOf(votedBy);
-			votes.splice(index, 1);
-		}
 	},
 	getters: {
 		getPlayerByID: (state) => (playerID) => {
