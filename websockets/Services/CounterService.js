522ee25d6920f077978869dbff974eeec5a7e2aa
moon250 <mooneupho@gmail.com>
2022-12-03 17:59:05 +0100

    fix: clearTimeout of game states cycle when game is deleted

--- websockets/Services/CounterService.js
+++ websockets/Services/CounterService.js
@@ -1,21 +1,31 @@
 import { StateManager } from "./StateManager.js";
 
 export class CounterService {
+	counterId = {};
+
 	constructor(io) {
 		this.io = io;
 		this.manager = new StateManager(this.io);
 	}
 
 	async cycle(channel, socket) {
-		{this.counterId = setTimeout(async () => {
+		const gameId = channel.split(".")[1];
+
+		const counterId = setTimeout(async () => {
 			await this.cycle(channel, socket);
-		}, ((await this.manager.getNextStateDuration(channel)) + 1) * 1000);}
+		}, ((await this.manager.getNextStateDuration(channel)) + 1) * 1000);
+
+		this.counterId[gameId] = counterId[Symbol.toPrimitive]();
 
 		try {
 			await this.manager.nextState(channel, this.counterId[Symbol.toPrimitive](), socket);
 		} catch (e) {
-			clearTimeout(this.counterId);
+			clearTimeout(this.counterId[gameId]);
 			console.error(e);
 		}
+	}
+
+	stop(gameId) {
+		clearTimeout(this.counterId[gameId]);
 	}
 }
