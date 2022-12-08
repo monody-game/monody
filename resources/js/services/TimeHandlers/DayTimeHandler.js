3c8c9f124f4a7b8f74ceae6066399006c2f01329
moon250 <mooneupho@gmail.com>
2022-12-08 21:38:34 +0100

    feat: add background change on client side

--- resources/js/services/TimeHandlers/DayTimeHandler.js
+++
@@ -1,9 +0,0 @@
-export default class DayTimeHandler {
-	switchBackround () {
-		const list = document.querySelector(".game-page__container").classList;
-		if (list.contains("night")) {
-			list.remove("night");
-		}
-		list.add("day");
-	}
-}
