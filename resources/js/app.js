ec5dc4cd43d7f84254730f21dd8f562d5c956774
moon250 <mooneupho@gmail.com>
2022-12-18 20:59:28 +0100

    feat: login page

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,6 +4,9 @@ import router from "./router/Router.js";
 import { createPinia } from "pinia";
 import "./bootstrap.js";
 import "../scss/style.scss";
+import SpinningDots from "@grafikart/spinning-dots-element";
+
+customElements.define("spinning-dots", SpinningDots);
 
 window.pinia = createPinia();
 
@@ -13,3 +16,4 @@ createApp(App)
 	.mount("#app");
 
 import "./Helpers.js";
+
