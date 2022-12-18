995d2e0e9b8223717f7c5043d233e895c7d335c6
moon250 <mooneupho@gmail.com>
2022-12-18 21:47:07 +0100

    feat: register page

--- resources/js/pages/Auth/RegisterPage.vue
+++ resources/js/pages/Auth/RegisterPage.vue
@@ -1,105 +1,112 @@
 <template>
-  <div class="register-page">
-    <h1>S'inscrire</h1>
-    <form class="register-page__form">
-      <div>
-        <input
-          v-model="username"
-          :class="errors.username.errored ? 'login-page__input-error' : ''"
-          class="register-page__input"
-          name="username"
-          placeholder="Nom d'utilisateur"
-          required
-          type="text"
+  <div class="auth-page__container">
+    <div class="auth-page__wrapper">
+      <div class="auth-page__form-wrapper">
+        <h1>S'inscrire</h1>
+        <form
+          class="register-page__form"
+          method="post"
+          action=""
+          @submit.prevent
         >
-        <span
-          class="login-page__input-focused"
-          :class="errors.username.errored ? 'login-page__input-focus-errored' : ''"
-        />
-        <p
-          v-if="errors.username.errored"
-          class="login-page__error"
-        >
-          {{ errors.username.text }}
-        </p>
+          <div
+            v-if="loading"
+            class="auth-page__loading-group"
+          >
+            <div class="auth-page__loading-group-blur" />
+            <DotsSpinner />
+          </div>
+          <div
+            class="auth-page__form-group"
+            :data-is-invalid="errors.username.errored"
+          >
+            <label for="username">Nom d'utilisateur</label>
+            <input
+              id="username"
+              v-model="username"
+              type="text"
+              name="username"
+            >
+            <svg v-if="errors.username.errored">
+              <use href="/sprite.svg#error" />
+            </svg>
+            <p v-if="errors.username.errored">
+              {{ errors.username.text }}
+            </p>
+          </div>
+          <div
+            class="auth-page__form-group"
+            :data-is-invalid="errors.email.errored"
+          >
+            <label for="email">Email</label>
+            <input
+              id="email"
+              v-model="email"
+              type="email"
+              name="email"
+            >
+            <svg v-if="errors.email.errored">
+              <use href="/sprite.svg#error" />
+            </svg>
+            <p v-if="errors.email.errored">
+              {{ errors.email.text }}
+            </p>
+          </div>
+          <div
+            class="auth-page__form-group"
+            :data-is-invalid="errors.password.errored"
+          >
+            <label for="password">Mot de passe</label>
+            <input
+              id="password"
+              v-model="password"
+              type="password"
+              name="password"
+            >
+            <svg v-if="errors.password.errored">
+              <use href="/sprite.svg#error" />
+            </svg>
+            <p v-if="errors.password.errored">
+              {{ errors.password.text }}
+            </p>
+          </div>
+          <div
+            class="auth-page__form-group"
+            :data-is-invalid="errors.password_confirmation.errored"
+          >
+            <label for="password_confirmation">Confirmez le mot de passe</label>
+            <input
+              id="password_confirmation"
+              v-model="password_confirmation"
+              type="password"
+              name="password_confirmation"
+            >
+            <svg v-if="password !== password_confirmation">
+              <use href="/sprite.svg#error" />
+            </svg>
+            <p v-if="password !== password_confirmation">
+              La confirmation du mot de passe doit être identique au mot de passe
+            </p>
+          </div>
+          <div class="auth-page__submit-group">
+            <router-link
+              class="auth-page__link"
+              to="login"
+            >
+              Vous possédez déjà un compte ?
+            </router-link>
+            <button
+              class="btn large"
+              type="submit"
+              :disabled="username === '' || email === '' || password === '' || password_confirmation === ''"
+              @click="register"
+            >
+              S'inscrire
+            </button>
+          </div>
+        </form>
       </div>
-      <div>
-        <input
-          v-model="email"
-          :class="errors.email.errored ? 'login-page__input-error' : ''"
-          class="register-page__input"
-          name="email"
-          placeholder="Email"
-          required
-          type="email"
-        >
-        <span
-          class="login-page__input-focused"
-          :class="errors.email.errored ? 'login-page__input-focus-errored' : ''"
-        />
-        <p
-          v-if="errors.email.errored"
-          class="login-page__error"
-        >
-          {{ errors.email.text }}
-        </p>
-      </div>
-      <div>
-        <input
-          v-model="password"
-          :class="errors.password.errored ? 'login-page__input-error' : ''"
-          class="register-page__input"
-          name="password"
-          placeholder="Mot de passe"
-          required
-          type="password"
-        >
-        <span
-          class="login-page__input-focused"
-          :class="errors.password.errored ? 'login-page__input-focus-errored' : ''"
-        />
-        <p
-          v-if="errors.password.errored"
-          class="login-page__error"
-        >
-          {{ errors.password.text }}
-        </p>
-      </div>
-      <div>
-        <input
-          v-model="password_confirmation"
-          :class="errors.password_confirmation.errored ? 'login-page__input-error' : ''"
-          class="register-page__input"
-          name="password_confirmation"
-          placeholder="Confirmez le mot de passe"
-          required
-          type="password"
-        >
-        <span
-          class="login-page__input-focused"
-          :class="errors.password_confirmation.errored ? 'login-page__input-focus-errored' : ''"
-        />
-        <p
-          v-if="errors.password_confirmation.errored"
-          class="login-page__error"
-        >
-          {{ errors.password_confirmation.text }}
-        </p>
-      </div>
-      <div>
-        <button
-          class="register-page__button"
-          type="submit"
-          @keyup.enter="register()"
-          @click.prevent="register()"
-        >
-          <DotsSpinner
-            v-if="loading === true"
-          />
-          S'inscrire
-        </button>
-      </div>
-    </form>
+    </div>
   </div>
 </template>
 
@@ -114,8 +121,8 @@ const email = ref("");
 const password = ref("");
 const password_confirmation = ref("");
 const loading = ref(false);
+
 const errors = ref({
-	text: "",
 	username: {
 		errored: false,
 		text: ""
@@ -134,14 +141,43 @@ const errors = ref({
 	}
 });
 
-watch(username, () => validate(username.value, "username"));
-watch(email, () => validate(email.value, "email"));
-watch(password, () => validate(password.value, "password"));
+watch(username, (newUsername) => {
+	if (newUsername.length > 24) {
+		errors.value.username.errored = true;
+		errors.value.username.text = "Votre nom d'utilsateur doit faire moins de 24 caractères";
+	} else if (newUsername.includes(" ")) {
+		errors.value.username.errored = true;
+		errors.value.username.text = "Il ne doit pas y avoir d'espaces dans votre pseudo";
+	} else {
+		errors.value.username.errored = false;
+		errors.value.username.text = "";
+	}
+});
+
+watch(password, (newPassword) => {
+	if (newPassword.length < 8) {
+		errors.value.password.errored = true;
+		errors.value.password.text = "Votre mot-de-passe doit faire plus de 8 caractères";
+	} else {
+		errors.value.password.errored = false;
+		errors.value.password.text = "";
+	}
+});
+
+watch(email, (newEmail) => {
+	if (newEmail.match(/^([a-z.0-9]+)@([a-z]+)\.([a-z]+)$/gm) === null) {
+		errors.value.email.errored = true;
+		errors.value.email.text = "Veuillez rentrer un email valide";
+	} else {
+		errors.value.email.errored = false;
+		errors.value.email.text = "";
+	}
+});
 
 const register = async function() {
 	if (checkInput()) {
 		loading.value = true;
-		await window
+		const res = await window
 			.JSONFetch("/auth/register", "POST", {
 				username: username.value,
 				email: email.value,
@@ -149,15 +185,27 @@ const register = async function() {
 				password_confirmation: password_confirmation.value,
 			});
 		loading.value = false;
+
+		if (res.status === 422) {
+			const validationErrors = res.data.errors;
+			for (const field in validationErrors) {
+				if (validationErrors[field].includes("validation.unique")) {
+					errors.value[field].text = "Un compte utilisant cette valeur existe déjà";
+					errors.value[field].errored = true;
+				}
+			}
+			return;
+		}
+
 		await router.push("play");
 	}
 };
 
 const checkInput = function () {
-	errors.value.username.errored = username.value === "";
-	errors.value.email.errored = email.value === "";
-	errors.value.password.errored = password.value === "";
-	errors.value.password_confirmation.errored = password_confirmation.value === "";
+	errors.value.username.errored = errors.value.username.errored || username.value === "";
+	errors.value.email.errored = errors.value.email.errored || email.value === "";
+	errors.value.password.errored = errors.value.password.errored || password.value === "";
+	errors.value.password_confirmation.errored = errors.value.password_confirmation.errored || password_confirmation.value === "";
 
 	if (username.value === "" || email.value === "" || password.value === "" || password_confirmation.value === "") {
 		errors.value.text = "Merci de remplir tous les champs";
@@ -165,44 +213,6 @@ const checkInput = function () {
 		return false;
 	}
 
-	if (password.value !== password_confirmation.value) {
-		errors.value.password.errored = true;
-		errors.value.password_confirmation.errored = true;
-		errors.value.text = "Les mots-de-passe doivent être identiques";
-		loading.value = false;
-		return false;
-	}
 	return true;
-};
-
-const validate = function (data, type) {
-	if (type === "email") {
-		if (data.match(/^([a-z.]+)@([a-z]+)\.([a-z]+)$/gm) === null) {
-			errors.value.email.errored = true;
-			errors.value.text = "Veuillez rentrer un email valide";
-		} else {
-			errors.value.email.errored = false;
-			errors.value.text = "";
-		}
-	}
-	if (type === "username") {
-		if (data.length > 24) {
-			errors.value.username.errored = true;
-			errors.value.text = "Votre pseudo doit faire moins de 24 caractères";
-		} else {
-			errors.value.username.errored = false;
-			errors.value.text = "";
-		}
-	}
-
-	if (type === "password") {
-		if (data.length < 6) {
-			errors.value.password.errored = true;
-			errors.value.text = "Votre mot-de-passe doit faire plus de 6 caractères";
-		} else {
-			errors.value.password.errored = false;
-			errors.value.text = "";
-		}
-	}
 };
 </script>
