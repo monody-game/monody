b3caa71d9c875eafc4b55ec5939d1894fe1f98fa
moon250 <mooneupho@gmail.com>
2023-01-09 21:23:27 +0100

    feat: witch hook

---
+++ websockets/Hooks/WitchHook.js
@@ -0,0 +1,15 @@
+import { InteractionService } from "../Services/InteractionService.js";
+
+export default {
+	identifier: 4,
+	async before(io, channel) {
+		await InteractionService.openInteraction(io, channel, "witch");
+
+		return false;
+	},
+	async after(io, channel) {
+		await InteractionService.closeInteraction(io, channel, "witch");
+
+		return false;
+	}
+};
