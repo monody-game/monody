38a5fce452a79b9ce858888e3556cc9f81a8710e
moon250 <mooneupho@gmail.com>
2023-01-21 17:52:44 +0100

    chore: op graph meta tags

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,8 +3,25 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <meta name="description" content="Monody est un jeu du loup-garou en ligne !"/>
-    <title>Monody</title>
+
+	<!-- Primary Meta Tags -->
+	<title>Monody</title>
+	<meta name="title" content="Monody">
+	<meta name="description" content="Monody est un jeu du loup-garou en ligne ! Jouez avec vos amis, débusquez les traîtres ou éliminez le village afin de remporter la partie !">
+
+	<!-- Open Graph / Facebook -->
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="https://monody.fr/">
+	<meta property="og:title" content="Monody">
+	<meta property="og:description" content="Monody est un jeu du loup-garou en ligne ! Jouez avec vos amis, débusquez les traîtres ou éliminez le village afin de remporter la partie !">
+	<meta property="og:image" content="https://monody.fr/images/monody.png">
+
+	<!-- Twitter -->
+	<meta property="twitter:card" content="summary_large_image">
+	<meta property="twitter:url" content="https://monody.fr/">
+	<meta property="twitter:title" content="Monody">
+	<meta property="twitter:description" content="Monody est un jeu du loup-garou en ligne ! Jouez avec vos amis, débusquez les traîtres ou éliminez le village afin de remporter la partie !">
+	<meta property="twitter:image" content="https://monody.fr/images/monody.png">
 
 	@vite('resources/js/app.js')
 </head>
