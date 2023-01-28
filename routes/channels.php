ac59103acd9b0b49677ca736ac72ca7af18eef42
moon250 <mooneupho@gmail.com>
2023-01-28 19:56:11 +0100

    style: run fixer

--- routes/channels.php
+++ routes/channels.php
@@ -3,7 +3,6 @@
 use App\Facades\Redis;
 use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
-use Illuminate\Support\Facades\Log;
 
 Broadcast::channel('home', function () {
     return true;
@@ -11,8 +10,8 @@ Broadcast::channel('home', function () {
 
 Broadcast::channel('game.{gameId}', function (User $user, $gameId) {
     $game = Redis::get("game:$gameId");
-	$members = Redis::get("game:$gameId:members") ?? [];
-	$member = array_filter($members, fn ($member) => $member['user_id'] === $user->id);
+    $members = Redis::get("game:$gameId:members") ?? [];
+    $member = array_filter($members, fn ($member) => $member['user_id'] === $user->id);
 
     if ($game === null || ($game['is_started'] && count($member) >= 1)) {
         return false;
