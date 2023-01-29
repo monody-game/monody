c0904d712f841e834602f824f327b64ae46a5102
moon250 <mooneupho@gmail.com>
2023-01-29 15:33:06 +0100

    feat: add stats and outcome tables/models

---
+++ app/Models/GameOutcome.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class GameOutcome extends Model
+{
+    protected $table = 'game_outcome';
+
+    public $timestamps = false;
+}
---
+++ app/Models/Statistic.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Concerns\HasUuids;
+use Illuminate\Database\Eloquent\Model;
+
+class Statistic extends Model
+{
+    protected $table = 'game_outcome';
+
+    protected $primaryKey = 'user_id';
+
+    use HasUuids;
+
+    public $timestamps = false;
+}
