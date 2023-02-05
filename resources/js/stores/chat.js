fa6f62de20f3248200e427b111d590b67bdad5e3
moon250 <mooneupho@gmail.com>
2023-02-05 14:42:08 +0100

    fix: chat messages scroll

--- resources/js/stores/chat.js
+++ resources/js/stores/chat.js
@@ -1,4 +1,5 @@
 import { defineStore } from "pinia";
+import { nextTick } from "vue";
 
 export const useStore = defineStore("chat", {
 	state: () => {
@@ -12,7 +13,13 @@ export const useStore = defineStore("chat", {
 				content,
 				type,
 				author,
-				actionList
+				actionList,
+				timestamp: Date.now()
+			});
+
+			nextTick(() => {
+				const messageContainer = document.querySelector(".chat__messages");
+				messageContainer.scrollTo(0, messageContainer.scrollHeight);
 			});
 		}
 	}
