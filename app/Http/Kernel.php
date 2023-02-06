a0336e8d00753be427c38a405d209ee024050623
moon250 <mooneupho@gmail.com>
2023-02-06 18:18:54 +0100

    fix: glide url in chat message

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -2,7 +2,6 @@
 
 namespace App\Http;
 
-use App\Http\Middleware\AddContentSecurityPolicyHeaders;
 use App\Http\Middleware\AddTokenToRequests;
 use App\Http\Middleware\Authenticate;
 use App\Http\Middleware\EncryptCookies;
