ad5b0be2ed272de017d586536daef2a63d67efa8
moon250 <mooneupho@gmail.com>
2023-02-06 17:37:43 +0100

    feat: implement glide

--- routes/web.php
+++ routes/web.php
@@ -4,4 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/{any}', function () {
     return view('app');
-})->where('any', '(?!api|storage).*');
+})->where('any', '(?!api|storage|assets).*');
+
+Route::get('/assets/{path}', 'ImageController@show')->where('path', '.*');
