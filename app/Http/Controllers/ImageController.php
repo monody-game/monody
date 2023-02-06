cef68da7346129e44fc8a088de7e92f360335126
moon250 <mooneupho@gmail.com>
2023-02-06 17:37:43 +0100

    feat: implement glide

---
+++ app/Http/Controllers/ImageController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Filesystem\Filesystem;
+use League\Glide\Responses\LaravelResponseFactory;
+use League\Glide\ServerFactory;
+
+class ImageController extends Controller
+{
+    public function show(Filesystem $filesystem, string $path): mixed
+    {
+		$server = ServerFactory::create([
+			'max_image_size' => 1000*1000,
+			'response' => new LaravelResponseFactory(app('request')),
+			'source' => $filesystem->getDriver(),
+			'cache' => $filesystem->getDriver(),
+			'cache_path_prefix' => '.cache',
+			'base_url' => 'img',
+		]);
+
+		return $server->getImageResponse($path, request()->all());
+    }
+}
