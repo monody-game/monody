c69ee840f7140872b93ec4399802f44dc2747277
moon250 <mooneupho@gmail.com>
2023-02-08 22:23:38 +0100

    feat: avatar uploading

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,6 +6,8 @@ use App\Services\RedisService;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Vite;
 use Illuminate\Support\ServiceProvider;
+use League\Glide\Responses\LaravelResponseFactory;
+use League\Glide\ServerFactory;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,6 +20,19 @@ class AppServiceProvider extends ServiceProvider
         $this->app->singleton(RedisService::class, function () {
             return new RedisService();
         });
+
+		$this->app->singleton('League\Glide\Server', function () {
+			$filesystem = app('Illuminate\Contracts\Filesystem\Filesystem');
+
+			return ServerFactory::create([
+				'max_image_size' => 1000 * 1000,
+				'response' => new LaravelResponseFactory(app('request')),
+				'source' => $filesystem->getDriver(),
+				'cache' => $filesystem->getDriver(),
+				'cache_path_prefix' => '.cache',
+				'base_url' => 'img',
+			]);
+		});
     }
 
     public function boot(): void
