74cb0d221be0deb51834a31b09115c51acdb98b7
moon250 <mooneupho@gmail.com>
2023-02-15 22:53:35 +0100

    feat: add next level exp to exp controller response

--- app/Http/Controllers/Api/ExpController.php
+++ app/Http/Controllers/Api/ExpController.php
@@ -4,21 +4,24 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use App\Models\Exp;
+use App\Models\User;
+use App\Services\ExpService;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class ExpController extends Controller
 {
-    public function get(Request $request): JsonResponse
+    public function get(Request $request, ExpService $service): JsonResponse
     {
-        /** @var string $user_id */
-        $user_id = $request->user()?->id;
+        /** @var User $user */
+        $user = $request->user();
 
-        $exp = Exp::firstOrCreate(['user_id' => $user_id]);
+        $exp = Exp::firstOrCreate(['user_id' => $user->id]);
 
         return new JsonResponse([
             'user_id' => $exp->user_id,
             'exp' => $exp->exp,
+            'next_level' => $service->nextLevelExp($user->level),
         ]);
     }
 }
