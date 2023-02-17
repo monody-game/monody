f73d80af4de0a12a33882db5302405b02aea08fe
moon250 <mooneupho@gmail.com>
2023-02-17 19:53:07 +0100

    fix: assets cache

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -3,11 +3,15 @@
 namespace App\Http\Controllers;
 
 use League\Glide\Server;
+use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class ImageController extends Controller
 {
-    public function show(Server $server, string $path): mixed
-    {
-        return $server->getImageResponse($path, request()->all());
+    public function show(Server $server, string $path): StreamedResponse
+	{
+		/** @var StreamedResponse $res */
+        $res = $server->getImageResponse($path, request()->all());
+		$res->setMaxAge(5 * 60);
+		return $res;
     }
 }
