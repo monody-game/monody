8bbce13ec69292291b6ef6362868f5e4931161c6
moon250 <mooneupho@gmail.com>
2023-02-17 18:51:49 +0100

    ref: exp notification subscription

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -12,10 +12,73 @@
 </template>
 
 <script setup>
+import { watch } from "vue";
+import { useStore } from "./stores/modals/popup.js";
+import { useStore as useUserStore } from "./stores/user.js";
+import { useStore as useAlertStore } from "./stores/alerts.js";
+import DebugBar from "./Components/DebugBar.vue";
 import AlertList from "./Components/Alerts/AlertList.vue";
 import PopupComponent from "./Components/Alerts/PopupComponent.vue";
-import { useStore } from "./stores/modals/popup.js";
-import DebugBar from "./Components/DebugBar.vue";
+import confetti from "canvas-confetti";
 
 const popupStore = useStore();
+const userStore = useUserStore();
+const alertStore = useAlertStore();
+
+watch(userStore, () => subscribeToChannel());
+
+const subscribeToChannel = () => {
+	if (Object.hasOwn(window.Echo.connector.channels, `private-App.Models.User.${userStore.id}`)) return;
+
+	window.Echo.private("App.Models.User." + userStore.id)
+		.notification((notification) => {
+			switch (notification.data.type) {
+			case "exp.earn":
+				if (notification.data.amount > 0 && notification.data.amount - userStore.exp > 0) {
+					alertStore.addAlerts({
+						"level": `Vous venez de gagner ${notification.data.amount - userStore.exp} exp`
+					});
+				}
+
+				userStore.exp = notification.data.amount;
+				break;
+			case "exp.levelup":
+				userStore.exp_needed = notification.data.exp_needed;
+				userStore.level = notification.data.level;
+				popupStore.setPopup({
+					level: {
+						title: "Bravo !",
+						content: `Vous venez de passer niveau ${userStore.level} ! Continuez ainsi !`,
+						note: "Accumulez de l'expérience en jouant sur Monody !"
+					}
+				});
+
+				if (window.matchMedia("(prefers-reduced-motion: reduce)") === false || window.matchMedia("(prefers-reduced-motion: reduce)").matches === false) {
+					startParty();
+				}
+			}
+		});
+};
+
+const startParty = () => {
+	const duration = 2 * 1000;
+	const animationEnd = Date.now() + duration;
+	const defaults = { startVelocity: 30, spread: 360, ticks: 60, zIndex: 10000 };
+
+	function randomInRange(min, max) {
+		return Math.random() * (max - min) + min;
+	}
+
+	const interval = setInterval(function() {
+		const timeLeft = animationEnd - Date.now();
+
+		if (timeLeft <= 0) {
+			return clearInterval(interval);
+		}
+
+		const particleCount = 50 * (timeLeft / duration);
+		confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.1, 0.3), y: Math.random() - 0.2 } }));
+		confetti(Object.assign({}, defaults, { particleCount, origin: { x: randomInRange(0.7, 0.9), y: Math.random() - 0.2 } }));
+	}, 250);
+};
 </script>
