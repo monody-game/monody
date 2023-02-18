36f23450c4a02723d27a853bbb6d72e2315a5eb8
moon250 <mooneupho@gmail.com>
2023-02-18 12:27:18 +0100

    fix: handle not found case in image controller

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -2,17 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\JsonResponse;
+use League\Glide\Filesystem\FileNotFoundException;
 use League\Glide\Server;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\StreamedResponse;
 
 class ImageController extends Controller
 {
-    public function show(Server $server, string $path): StreamedResponse
+    public function show(Server $server, string $path): Response
     {
-        /** @var StreamedResponse $res */
-        $res = $server->getImageResponse($path, request()->all());
-        $res->setMaxAge(5 * 60);
+		try {
+			/** @var StreamedResponse $res */
+			$res = $server->getImageResponse($path, request()->all());
+			$res->setMaxAge(5 * 60);
 
-        return $res;
+			return $res;
+		} catch (FileNotFoundException $e) {
+			return new JsonResponse([
+				'message' => 'File not found.'
+			], Response::HTTP_NOT_FOUND);
+		}
     }
 }
