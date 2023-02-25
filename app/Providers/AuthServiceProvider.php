4152883e1bab187535ecafb514b1c7e49986ba7f
moon250 <mooneupho@gmail.com>
2023-02-25 18:40:05 +0100

    ref: password reset notification

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,8 +2,6 @@
 
 namespace App\Providers;
 
-use App\Models\User;
-use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Notifications\Messages\MailMessage;
@@ -21,19 +19,6 @@ class AuthServiceProvider extends ServiceProvider
                 ->subject('Monody | Vérifiez votre adresse mail')
                 ->line('Cliquez sur le bouton ci-dessous afin de vérifier votre adresse mail.')
                 ->action('Vérifier', $url);
-        });
-
-        ResetPassword::createUrlUsing(function (User $user, string $token) {
-            return config('app.url') . '/reset/' . $token;
-        });
-
-        ResetPassword::toMailUsing(function ($notifiable, $url) {
-            return (new MailMessage)
-                ->subject('Monody | Réinitialiser votre mot de passe')
-                ->line('Cliquez sur le bouton ci-dessous afin de changer votre mot de passe.')
-                ->action('Réinitialiser le mot de passe', $url)
-                ->line('Ce lien va expirer dans ' . config('auth.passwords.users.expire') . ' minutes.')
-                ->line('Vous n\'êtes pas à l\'origine de cette demande ? Ignorez simplement ce mail.');
         });
     }
 }
