864e101e5401a241211204da50e8964dfe0d86e6
moon250 <mooneupho@gmail.com>
2023-03-05 17:03:36 +0100

    feat: add user avatar to profile

--- app/Http/Controllers/Api/ShareController.php
+++ app/Http/Controllers/Api/ShareController.php
@@ -12,17 +12,31 @@ use Illuminate\Support\Facades\Storage;
 use Intervention\Image\AbstractFont;
 use Intervention\Image\AbstractShape;
 use Intervention\Image\Facades\Image;
+use League\Glide\Server;
 
 class ShareController extends Controller
 {
-    public function index(Request $request, ExpService $expService): JsonResponse
+    public function index(Request $request, ExpService $expService, Server $glide): JsonResponse
     {
         /** @var User $user route is protected by api auth guard */
         $user = $request->user();
-        $avatar = str_replace('/assets/', '', $user->avatar);
-        $avatar = Image::make(Storage::path($avatar));
+        $avatarPath = str_replace('/assets/', '', $user->avatar);
 
-        Image::cache(function ($image) use ($user, $expService) {
+        $avatar = Image::cache(function ($image) use ($avatarPath, $user) {
+            $avatar = $image->make(Storage::path($avatarPath));
+            $avatar->fit(1250, 1250);
+            $avatar->mask($this->createCircleMask(1250, 1250), false);
+            $avatar->trim('top-left');
+            $avatar->save(Storage::path("profiles/{$user->id}-avatar.temp.png"));
+
+            Storage::delete("profiles/{$user->id}-avatar.temp.png");
+
+            return $avatar;
+        });
+
+        Image::cache(function ($image) use ($user, $expService, $avatar, $avatarPath, $glide) {
+            $glide->deleteCache(str_replace('avatars', 'profiles', $avatarPath));
+
             $exp = Exp::where('user_id', $user->id)->get()[0]?->exp;
             $neededExp = $expService->nextLevelExp($user->level);
 
@@ -34,20 +48,17 @@ class ShareController extends Controller
             $darkBorder = 'rgba(15, 17, 39, .25)';
             $accentLight = 'fff5cf';
 
-            $image = $image->make(Storage::path('profiles/template.png'));
+            $profile = $image->make(Storage::path('profiles/template.png'));
 
-            $image->circle(1250, 785, 785, function (AbstractShape $shape) use ($darkBackground) {
-                // TODO: Use avatar as background (needs rework)
-                $shape->background($darkBackground);
-            });
+            $profile->insert($avatar, 'top-left', 160, 160);
 
-            $image->text($user->username, 1570, 160 + 190, fn (AbstractFont $font) => $font->file($senBold)->size(200)->color($darkBackground));
+            $profile->text($user->username, 1570, 160 + 190, fn (AbstractFont $font) => $font->file($senBold)->size(200)->color($darkBackground));
 
-            $image->text($user->level, 2340, 630 + 110, fn (AbstractFont $font) => $font->file($senRegular)->size(120)->color($darkBackground));
+            $profile->text($user->level, 2340, 630 + 110, fn (AbstractFont $font) => $font->file($senRegular)->size(120)->color($darkBackground));
 
-            $image->text($elo, 3190, 630 + 110, fn (AbstractFont $font) => $font->file($senRegular)->size(120)->color($darkBackground));
+            $profile->text($elo, 3190, 630 + 110, fn (AbstractFont $font) => $font->file($senRegular)->size(120)->color($darkBackground));
 
-            $image->rectangle(1570, 970, 1570 + 2055, 970 + 240, fn (AbstractShape $shape) => $shape->border(10, $darkBorder));
+            $profile->rectangle(1570, 970, 1570 + 2055, 970 + 240, fn (AbstractShape $shape) => $shape->border(10, $darkBorder));
 
             $progressLength = ($exp * (1580 + 2040)) / $neededExp;
 
@@ -55,15 +66,22 @@ class ShareController extends Controller
                 $progressLength = 1580;
             }
 
-            $image->rectangle(1575, 975, $progressLength, 965 + 240, fn (AbstractShape $shape) => $shape->background($accentLight));
+            $profile->rectangle(1575, 975, $progressLength, 965 + 240, fn (AbstractShape $shape) => $shape->background($accentLight));
 
-            $image->text($exp . '/' . $neededExp, 2477, 1015 + 110, fn (AbstractFont $font) => $font->file($senRegular)->size(120));
+            $profile->text($exp . '/' . $neededExp, 2477, 1015 + 110, fn (AbstractFont $font) => $font->file($senRegular)->size(120));
 
             $image->widen(1280);
 
-            $image->save(Storage::path("profiles/{$user->id}.png"));
+            $profile->save(Storage::path("profiles/{$user->id}.png"));
         });
 
         return new JsonResponse();
+    }
+
+    public function createCircleMask(int $width, int $height): \Intervention\Image\Image
+    {
+        $circle = Image::canvas($width, $height);
+
+        return $circle->circle($width - 1, $width / 2, $height / 2, fn (AbstractShape $shape) => $shape->background('fff'));
     }
 }
