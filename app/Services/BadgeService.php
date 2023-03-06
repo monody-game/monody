5b47d44e794dd15f31aeb85e040e4f779cacfe6c
moon250 <mooneupho@gmail.com>
2023-03-06 21:52:59 +0100

    feat: add badge service

---
+++ app/Services/BadgeService.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Services;
+
+use App\Enums\Badges;
+use App\Models\Badge;
+use App\Models\User;
+
+class BadgeService
+{
+    /**
+     * Retrieve user's badges
+     *
+     * For example, user with Beta and Wins (level 3) badges, this function will return :
+     *
+     * [
+     * 		0 => [
+     * 			"badge" => Badges::Beta,
+     * 			"user_id" => "...",
+     * 			"level" => 0,
+     * 			"obtained_at" => "1970-01-01 00:00:00",
+     * 		],
+     * 		1 => [
+     * 			"badge" => Badges::Wins,
+     * 			"user_id" => "...",
+     * 			"level" => 3,
+     * 			"obtained_at" => "1970-01-01 00:00:00",
+     * 		],
+     * ]
+     *
+     * @return array<int, array{badge: Badges, user_id: string, level: int, obtained_at: string}>
+     */
+    public function get(User $user): array
+    {
+        $badges = Badge::where('user_id', $user->id)->get();
+
+        $badges = $badges->map(function (Badge $badge) {
+            return [
+                'badge' => Badges::from($badge->badge_id),
+                'user_id' => $badge->user_id,
+                'level' => $badge->level,
+                'obtained_at' => $badge->obtained_at,
+            ];
+        });
+
+        return $badges->toArray();
+    }
+
+    /**
+     * Grant a badge to an user with automatic level determination
+     */
+    public function add(User $user, Badges $badge, int $level = 1): void
+    {
+        $hasBadge = Badge::where('user_id', $user->id)->where('badge_id', $badge->value)->get();
+
+        if ($hasBadge->count() > 0 && $badge->maxLevel() <= $hasBadge->count() + 1) {
+            $level = $hasBadge->count() + 1;
+        }
+
+        if ($badge->maxLevel() === 0) {
+            $level = 0;
+        }
+
+        $model = new Badge();
+        $model->user_id = $user->id;
+        $model->badge_id = $badge->value;
+        $model->level = $level;
+        $model->save();
+    }
+}
---
+++ tests/Unit/Services/BadgeServiceTest.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace Tests\Unit\Services;
+
+use App\Enums\Badges;
+use App\Models\Badge;
+use App\Models\User;
+use App\Services\BadgeService;
+use Illuminate\Support\Carbon;
+use Tests\TestCase;
+
+class BadgeServiceTest extends TestCase
+{
+    public function testGettingUserBadges()
+    {
+        $service = new BadgeService();
+        $user = User::factory()->create();
+
+        $this->assertSame([], $service->get($user));
+
+        $badge = new Badge([
+            'user_id' => $user->id,
+            'badge_id' => Badges::Owner->value,
+            'level' => 0,
+            'obtained_at' => '1970-01-01 00:00:00',
+        ]);
+        $badge->save();
+
+        $badge = new Badge([
+            'user_id' => $user->id,
+            'badge_id' => Badges::Wins->value,
+            'level' => 4,
+            'obtained_at' => '1970-01-01 00:00:00',
+        ]);
+        $badge->save();
+
+        $this->assertSame([
+            [
+                'badge' => Badges::Owner,
+                'user_id' => $user->id,
+                'level' => 0,
+                'obtained_at' => '1970-01-01 00:00:00',
+            ],
+            [
+                'badge' => Badges::Wins,
+                'user_id' => $user->id,
+                'level' => 4,
+                'obtained_at' => '1970-01-01 00:00:00',
+            ],
+        ], $service->get($user));
+    }
+
+    public function testAddingBadge()
+    {
+        $service = new BadgeService();
+        $user = User::factory()->create();
+
+        $this->assertSame([], $service->get($user));
+
+        $service->add($user, Badges::Owner);
+
+        $this->assertSame([
+            [
+                'badge' => Badges::Owner,
+                'user_id' => $user->id,
+                'level' => 0,
+                'obtained_at' => Carbon::now()->toDateString() . ' ' . Carbon::now()->toTimeString(),
+            ],
+        ], $service->get($user));
+    }
+
+    public function testAddingMaxLevelBadge()
+    {
+        $service = new BadgeService();
+        $user = User::factory()->create();
+        $badges = collect([]);
+
+        for ($i = 1; $i < Badges::Wins->maxLevel(); $i++) {
+            $badges[] = $badge = new Badge([
+                'user_id' => $user->id,
+                'badge_id' => Badges::Wins->value,
+                'level' => $i,
+                'obtained_at' => '1970-01-01 00:00:00',
+            ]);
+            $badge->save();
+        }
+
+        $badges = $badges->map(function (Badge $badge) {
+            return [
+                'badge' => Badges::from($badge->badge_id),
+                'user_id' => $badge->user_id,
+                'level' => $badge->level,
+                'obtained_at' => $badge->obtained_at,
+            ];
+        });
+
+        $service->add($user, Badges::Wins);
+
+        $badges[] = [
+            'badge' => Badges::Wins,
+            'user_id' => $user->id,
+            'level' => 5,
+            'obtained_at' => Carbon::now()->toDateString() . ' ' . Carbon::now()->toTimeString(),
+        ];
+
+        $this->assertSame($badges->toArray(), $service->get($user));
+    }
+}
