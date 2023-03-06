0c0aaeb6d53860dc8ec3627b03d3a7ef56a5a12f
moon250 <mooneupho@gmail.com>
2023-03-06 21:20:21 +0100

    feat: add badges to enum

--- app/Enums/Badges.php
+++ app/Enums/Badges.php
@@ -4,5 +4,22 @@ namespace App\Enums;
 
 enum Badges: int
 {
+	// One-level badges
+	case Graphist = 0;
     case Beta = 1;
+	case Owner = 2;
+
+	// Other badges
+	case Wins = 3;
+	case Losses = 4;
+	case Level = 5;
+	case Rank = 6;
+
+	public function maxLevel(): int
+	{
+		return match ($this) {
+			self::Graphist, self::Beta, self::Owner => 0,
+			self::Wins, self::Losses, self::Level, self::Rank => 5
+		};
+	}
 }
