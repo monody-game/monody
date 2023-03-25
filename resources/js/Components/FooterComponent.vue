f530ad00424a6c5fa3a1e4b94c170e573841f131
moon250 <mooneupho@gmail.com>
2023-03-25 12:01:56 +0100

    feat: add socials to footer

--- resources/js/Components/FooterComponent.vue
+++ resources/js/Components/FooterComponent.vue
@@ -4,6 +4,22 @@
     class="footer__main"
   >
     <p>&copy; Monody {{ (new Date()).getFullYear() }} — Tous droits réservés.</p>
+    <a
+      href="https://www.instagram.com/monody_game"
+      target="_blank"
+    >
+      <svg>
+        <use href="/sprite.svg#instagram" />
+      </svg>
+    </a>
+    <a
+      href="https://discord.gg/FxeRt58xZt"
+      target="_blank"
+    >
+      <svg>
+        <use href="/sprite.svg#discord" />
+      </svg>
+    </a>
   </footer>
 </template>
 
