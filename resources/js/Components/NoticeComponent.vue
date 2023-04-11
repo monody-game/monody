145c6aa8ae52de26ad2bab34d57116bc4766e1b3
moon250 <mooneupho@gmail.com>
2023-04-11 12:17:31 +0200

    fix: hidden overflow on modals

--- resources/js/Components/NoticeComponent.vue
+++ resources/js/Components/NoticeComponent.vue
@@ -1,39 +1,33 @@
 <template>
-  <svg
-    ref="icon"
-    class="notice__icon"
-  >
-    <use href="/sprite.svg#question" />
-  </svg>
   <div
-    ref="content"
-    class="notice__content"
+    class="notice-component__wrapper"
+    @mouseover="content.style.display = 'grid'"
+    @mouseout="content.style.display = 'none'"
   >
-    <p class="notice__title">
-      {{ props.title }}
-    </p>
-    <p class="notice__description">
-      <slot />
-    </p>
+    <svg
+      class="notice__icon"
+    >
+      <use href="/sprite.svg#question" />
+    </svg>
+    <div
+      ref="content"
+      class="notice__content"
+    >
+      <p class="notice__title">
+        {{ props.title }}
+      </p>
+      <p class="notice__description">
+        <slot />
+      </p>
+    </div>
   </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from "vue";
+import { ref } from "vue";
 
 const props = defineProps({
 	title: String
 });
-const icon = ref(null);
 const content = ref(null);
-
-onMounted(() => {
-	icon.value.addEventListener("pointerenter", () => {
-		content.value.style.display = "grid";
-	});
-
-	icon.value.addEventListener("pointerleave", () => {
-		content.value.style.display = "none";
-	});
-});
 </script>
