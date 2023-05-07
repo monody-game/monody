b3bb01e17b096b9843109e05d996c9f0e9f2e6e0
moon250 <mooneupho@gmail.com>
2023-05-07 23:10:11 +0200

    fix: handle json deserialization errors

--- websockets/dist/Redis/RedisSubscriber.js
+++ websockets/dist/Redis/RedisSubscriber.js
@@ -1,5 +1,5 @@
 import { client } from "./Connection.js";
-import { log } from "../Logger.js";
+import { error, log } from "../Logger.js";
 export class RedisSubscriber {
     sub;
     constructor() {
@@ -8,12 +8,18 @@ export class RedisSubscriber {
     }
     async subscribe(callback) {
         await this.sub.pSubscribe("*", async (message, channel) => {
-            if (process.env.APP_DEBUG) {
-                log("Api emitted an event !");
-                log("Channel: " + channel);
-                log("Event: " + JSON.parse(message).event);
+            try {
+                const event = JSON.parse(message);
+                if (process.env.APP_DEBUG) {
+                    log("Api emitted an event !");
+                    log("Channel: " + channel);
+                    log("Event: " + event.event);
+                }
+                return await callback(channel, event);
             }
-            return await callback(channel, JSON.parse(message));
+            catch (e) {
+                error(e);
+            }
         });
     }
     async unsubscribe() {
