a562f0f7581993b004d20483411cefc7bfa33f4b
moon250 <mooneupho@gmail.com>
2023-05-09 18:19:46 +0200

    tryfix: add death announcement in hunter's before hook and add end game check

--- websockets/Hooks/HunterHook.ts
+++ websockets/Hooks/HunterHook.ts
@@ -3,19 +3,30 @@ import {Server} from "socket.io";
 import fetch from "../Helpers/fetch.js";
 import {gameId} from "../Helpers/Functions.js";
 
+const baseUrl = `${process.env.API_URL}/game`;
+const body = (channel: string) => { return { gameId: gameId(channel) } };
+
 export default {
 	identifier: 17,
 	async before(io: Server, channel: string) {
+		await fetch(`${baseUrl}/message/deaths`, "POST", body(channel));
+
 		await InteractionService.openInteraction(io, channel, "hunter");
 
 		return false;
 	},
 	async after(io: Server, channel: string) {
 		await InteractionService.closeInteraction(io, channel, "hunter");
-		const baseUrl = `${process.env.API_URL}/game`;
-		const body = { gameId: gameId(channel) };
 
-		await fetch(`${baseUrl}/message/deaths`, "POST", body);
+		await fetch(`${baseUrl}/message/deaths`, "POST", body(channel));
+
+		const res = await fetch(`${baseUrl}/end/check`, "POST", body);
+
+		if (res.status === 204) {
+			await fetch(`${baseUrl}/end`, "POST", body);
+
+			return true;
+		}
 
 		return false;
 	}
--- websockets/dist/Hooks/HunterHook.js
+++ websockets/dist/Hooks/HunterHook.js
@@ -1,17 +1,23 @@
 import { InteractionService } from "../Services/InteractionService.js";
 import fetch from "../Helpers/fetch.js";
 import { gameId } from "../Helpers/Functions.js";
+const baseUrl = `${process.env.API_URL}/game`;
+const body = (channel) => { return { gameId: gameId(channel) }; };
 export default {
     identifier: 17,
     async before(io, channel) {
+        await fetch(`${baseUrl}/message/deaths`, "POST", body(channel));
         await InteractionService.openInteraction(io, channel, "hunter");
         return false;
     },
     async after(io, channel) {
         await InteractionService.closeInteraction(io, channel, "hunter");
-        const baseUrl = `${process.env.API_URL}/game`;
-        const body = { gameId: gameId(channel) };
-        await fetch(`${baseUrl}/message/deaths`, "POST", body);
+        await fetch(`${baseUrl}/message/deaths`, "POST", body(channel));
+        const res = await fetch(`${baseUrl}/end/check`, "POST", body);
+        if (res.status === 204) {
+            await fetch(`${baseUrl}/end`, "POST", body);
+            return true;
+        }
         return false;
     }
 };
