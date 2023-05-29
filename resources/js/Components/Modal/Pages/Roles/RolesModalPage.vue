39aa2651cb9f6b389d91c9583b8e051c1090dce9
moon250 <mooneupho@gmail.com>
2023-05-29 22:08:55 +0200

    fix: role's limit broken

--- resources/js/Components/Modal/Pages/Roles/RolesModalPage.vue
+++ resources/js/Components/Modal/Pages/Roles/RolesModalPage.vue
@@ -123,6 +123,9 @@ const getRoles = async function() {
 
 		list.roles.forEach((role) => {
 			role.image = window.location.origin + role.image;
+			if (role.limit === -1) {
+				delete role.limit;
+			}
 		});
 
 		roles.value = list.roles;
