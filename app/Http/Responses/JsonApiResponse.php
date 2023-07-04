8b73c72d0cae23ba02cc56af669fe5d6452a57d5
moon250 <mooneupho@gmail.com>
2023-07-04 13:33:21 +0200

    feat: add cache object to meta object in default json response

--- app/Http/Responses/JsonApiResponse.php
+++ app/Http/Responses/JsonApiResponse.php
@@ -4,8 +4,10 @@ namespace App\Http\Responses;
 
 use App\Enums\AlertType;
 use App\Enums\Status;
+use Carbon\CarbonInterface;
 use Illuminate\Contracts\Support\Responsable;
 use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Carbon;
 use Symfony\Component\HttpFoundation\Cookie;
 
 class JsonApiResponse implements Responsable
@@ -16,18 +18,25 @@ class JsonApiResponse implements Responsable
         public ?array $alerts = null,
         public ?array $popups = null,
         public array $cookies = [],
-        public array $headers = []
+        public array $headers = [],
+        public array $cache = []
     ) {
     }
 
     public function toResponse($request): JsonResponse
     {
+        $cache = [
+            'cache' => array_key_exists('cache', $this->cache) ? $this->cache['cache'] : true,
+            'until' => array_key_exists('until', $this->cache) ? $this->cache['until'] : Carbon::now()->addDay(),
+        ];
+
         $response = new JsonResponse(
             data: [
                 'status' => $this->status->statusify(),
                 'meta' => [
                     'name' => config('app.name'),
                     'version' => config('app.version'),
+                    'cache' => $cache,
                 ],
                 'data' => $this->data,
                 'alerts' => $this->alerts,
@@ -76,6 +85,21 @@ class JsonApiResponse implements Responsable
     public function withCookie(Cookie $cookie): self
     {
         $this->cookies[] = $cookie;
+
+        return $this;
+    }
+
+    public function withoutCache(): self
+    {
+        $this->cache['cache'] = false;
+
+        return $this;
+    }
+
+    public function withCache(CarbonInterface $until): self
+    {
+        $this->cache['cache'] = true;
+        $this->cache['until'] = $until;
 
         return $this;
     }
