34aee10f6832fd4056d2c72bc5c10e3653587be2
moon250 <mooneupho@gmail.com>
2023-07-10 22:00:34 +0200

    ref: add more abstraction to event classes

--- app/Events/Abstract/DiscordBotEvent.php
+++ app/Events/Abstract/DiscordBotEvent.php
@@ -2,27 +2,9 @@
 
 namespace App\Events\Abstract;
 
-use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
 
-abstract class DiscordBotEvent implements ShouldBroadcastNow
+abstract class DiscordBotEvent extends AbstractEvent implements ShouldBroadcastNow
 {
     public static string $channel = 'bot.private';
-
-    protected string $event;
-
-    use Dispatchable, InteractsWithSockets, SerializesModels;
-
-    public function broadcastOn(): Channel
-    {
-        return new Channel(self::$channel);
-    }
-
-    public function broadcastAs(): string
-    {
-        return $this->event;
-    }
 }
