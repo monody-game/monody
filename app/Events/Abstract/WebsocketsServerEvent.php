d1071c6722b11fdca2a701313677cb115e6d9cde
moon250 <mooneupho@gmail.com>
2023-07-10 22:00:34 +0200

    ref: add more abstraction to event classes

--- app/Events/Abstract/WebsocketsServerEvent.php
+++ app/Events/Abstract/WebsocketsServerEvent.php
@@ -3,18 +3,11 @@
 namespace App\Events\Abstract;
 
 use Illuminate\Broadcasting\Channel;
-use Illuminate\Broadcasting\InteractsWithSockets;
 use Illuminate\Contracts\Broadcasting\ShouldBroadcastNow;
-use Illuminate\Foundation\Events\Dispatchable;
-use Illuminate\Queue\SerializesModels;
 
-abstract class WebsocketsServerEvent implements ShouldBroadcastNow
+abstract class WebsocketsServerEvent extends AbstractEvent implements ShouldBroadcastNow
 {
     public static string $channel = 'ws.private';
-
-    protected string $event;
-
-    use Dispatchable, InteractsWithSockets, SerializesModels;
 
     public function broadcastOn(): Channel
     {
