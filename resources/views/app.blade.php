529466e025d04765bce5b7451c89b565905b23ee
moon250 <mooneupho@gmail.com>
2023-07-17 15:52:49 +0200

    fix: favicons

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,6 +4,7 @@
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<meta name="theme-color" content="#0f1127">
+	<meta name="msapplication-TileColor" content="#0f1127">
 
 	<!-- Primary Meta Tags -->
 	<title>Monody</title>
@@ -11,6 +12,10 @@
 	<meta name="description"
 		  content="Monody est un jeu du loup-garou en ligne ! Jouez avec vos amis, débusquez les traîtres ou éliminez le village afin de remporter la partie !">
 	<link rel="shortcut icon" href="/favicon.ico" type="image/x-icon">
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#0f1127">
 
 	<!-- Open Graph / Facebook -->
 	<meta property="og:type" content="website">
