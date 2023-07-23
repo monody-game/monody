1e54141c4dcaf1d43bc9a21b778d418a06894981
moon250 <mooneupho@gmail.com>
2023-07-23 12:41:13 +0200

    feat: api i18n middleware

---
+++ app/Http/Middleware/Localization.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\App;
+use Symfony\Component\HttpFoundation\Response;
+
+class Localization
+{
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->header('Accept-Language') !== null) {
+            foreach (explode(';', $request->header('Accept-Language')) as $langugage) {
+                if (in_array($langugage, config('app.supported_locales'), true)) {
+                    App::setLocale($langugage);
+                    break;
+                }
+            }
+        }
+
+        return $next($request);
+    }
+}
