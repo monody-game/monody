9dce92fa26d296e1f5849b4d0727552f025845ff
moon250 <mooneupho@gmail.com>
2023-07-27 00:07:02 +0200

    ref: lock chat before role assignation

--- websockets/Hooks/RoleAssignementHook.ts
+++ websockets/Hooks/RoleAssignementHook.ts
@@ -3,10 +3,15 @@ import { InteractionService } from "../Services/InteractionService.js";
 import { gameId } from "../Helpers/Functions.js";
 import { client } from "../Redis/Connection.js";
 import { Server } from "socket.io";
+import fetch from "../Helpers/fetch.js";
 
 export default {
 	identifier: 9,
 	async before(io: Server, channel: string) {
+		await fetch(`${process.env.API_URL}/game/chat/lock/true`, "POST", {
+			gameId: gameId(channel),
+		});
+
 		await GameService.roleManagement(io, channel);
 		const game = JSON.parse(
 			(await client.get(`game:${gameId(channel)}`)) as string,
