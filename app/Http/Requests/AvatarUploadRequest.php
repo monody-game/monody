1440c28364d23df5ec902342eb45793132a130fc
moon250 <mooneupho@gmail.com>
2023-08-06 19:58:26 +0200

    fix: avatar upload error when avatar was too large

--- app/Http/Requests/AvatarUploadRequest.php
+++ app/Http/Requests/AvatarUploadRequest.php
@@ -2,14 +2,21 @@
 
 namespace App\Http\Requests;
 
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Foundation\Http\FormRequest;
 
 class AvatarUploadRequest extends FormRequest
 {
+	protected function failedValidation(Validator $validator)
+	{
+		dd(ini_get('post_max_size'), ini_get('upload_max_filesize'));
+		dd($validator);
+	}
+
     public function rules(): array
     {
         return [
-            'avatar' => 'required|file|max:2048|mimes:jpg,png,webp,bmp|dimensions:min_width=100,min_height=100,max_width=2500,max_height=2500',
+            'avatar' => 'required|file|max:4096|mimes:jpg,png,webp,bmp|dimensions:min_width=100,min_height=100,max_width=2500,max_height=2500',
         ];
     }
 }
--- resources/js/Components/Modal/ProfileModal.vue
+++ resources/js/Components/Modal/ProfileModal.vue
@@ -200,6 +200,14 @@ const formattedDate = computed(() => {
 });
 
 const addFile = () => {
+	if ((avatarInput.value.files[0].size / 1000) >= 4096) {
+		alertStore.addAlerts({
+			error: t('profile.avatar_too_large', [Math.floor(avatarInput.value.files[0].size / 1_000_000)])
+		})
+
+		return
+	}
+
 	if (avatarInput.value.files.length > 0) {
 		hasUploaded.value = true;
 	}
