52404f98a24dd39dfee91d60afd7677c87e53d4d
moon250 <mooneupho@gmail.com>
2023-08-20 14:39:53 +0200

    fix: missing translation and remove rank badge

--- resources/js/Components/PlayerPresentation/UserStatistics.vue
+++ resources/js/Components/PlayerPresentation/UserStatistics.vue
@@ -4,34 +4,31 @@
 		<DotsSpinner />
 	</div>
 	<div class="stats__container">
-		<div class="stats__statistic" title="Nombre de victoires">
+		<div class="stats__statistic">
 			<svg>
 				<use href="/sprite.svg#trophy" />
 			</svg>
 			<p>{{ $t("stats.wins", [stats.wins]) }}</p>
 		</div>
-		<div class="stats__statistic" title="Ratio de victoires">
+		<div class="stats__statistic">
 			<svg>
 				<use href="/sprite.svg#win_rate" />
 			</svg>
 			<p>{{ $t("stats.ratio", [winRate]) }}</p>
 		</div>
-		<div class="stats__statistic" title="Nombre de défaites">
+		<div class="stats__statistic">
 			<svg>
 				<use href="/sprite.svg#losses" />
 			</svg>
 			<p>{{ $t("stats.losses", [stats.losses]) }}</p>
 		</div>
-		<div class="stats__statistic" title="Série de victoires">
+		<div class="stats__statistic">
 			<svg>
 				<use href="/sprite.svg#win_streak" />
 			</svg>
 			<p>{{ $t("stats.streak", [stats.win_streak]) }}</p>
 		</div>
-		<div
-			class="stats__statistic stats__statistic-large"
-			title="Plus longue série de victoires"
-		>
+		<div class="stats__statistic stats__statistic-large">
 			<svg viewBox="0 0 30 35" fill="none" xmlns="http://www.w3.org/2000/svg">
 				<path
 					d="M22.5291 14.5782C22.1202 14.0448 21.6224 13.5826 21.1602 13.1204C19.969 12.0537 18.6179 11.2892 17.4801 10.1692C14.8311 7.57355 14.2444 3.28898 15.9334 0C14.2444 0.408901 12.7688 1.33337 11.5066 2.34673C6.90199 6.04462 5.0886 12.5692 7.25755 18.1694C7.32866 18.3472 7.39978 18.525 7.39978 18.7561C7.39978 19.1472 7.1331 19.5028 6.77754 19.645C6.36864 19.8228 5.94196 19.7161 5.60417 19.4317C5.50267 19.3477 5.41818 19.2451 5.35528 19.1294C3.34633 16.5871 3.02632 12.9426 4.37747 10.027C1.4085 12.4448 -0.209327 16.5338 0.0217904 20.3917C0.12846 21.2806 0.23513 22.1695 0.537361 23.0584C0.786257 24.1251 1.26627 25.1918 1.79962 26.1341C3.71967 29.2097 7.04421 31.4142 10.6176 31.8587C14.4222 32.3387 18.4934 31.6453 21.4091 29.0142C24.6625 26.063 25.8003 21.3339 24.1291 17.2805L23.898 16.8183C23.5247 16.0005 22.5291 14.5782 22.5291 14.5782ZM16.9112 25.7785C16.4134 26.2052 15.5956 26.6674 14.9555 26.8452C12.9644 27.5563 10.9732 26.5608 9.79985 25.3874C11.9155 24.8896 13.1777 23.3251 13.5511 21.7428C13.8533 20.3206 13.2844 19.1472 13.0533 17.7783C12.8399 16.4627 12.8755 15.3427 13.3555 14.116C13.6933 14.7915 14.0489 15.4671 14.4755 16.0005C15.8445 17.7783 17.9956 18.5605 18.4579 20.9784C18.529 21.2273 18.5645 21.4762 18.5645 21.7428C18.6179 23.2007 17.9779 24.8007 16.9112 25.7785Z"
@@ -54,10 +51,7 @@
 			</svg>
 			<p>{{ $t("stats.longest_streak", [stats.longest_streak]) }}</p>
 		</div>
-		<div
-			class="stats__statistic stats__statistic-large"
-			title="Rôle le plus possédé"
-		>
+		<div class="stats__statistic stats__statistic-large">
 			<img
 				:src="stats.most_possessed_role.image"
 				:alt="stats.most_possessed_role.display_name"
@@ -71,10 +65,7 @@
 				}}
 			</p>
 		</div>
-		<div
-			class="stats__statistic stats__statistic-large"
-			title="Rôle le plus victorieux"
-		>
+		<div class="stats__statistic stats__statistic-large">
 			<img
 				:src="stats.highest_win_role.image"
 				:alt="stats.highest_win_role.display_name"
@@ -94,6 +85,9 @@
 <script setup>
 import DotsSpinner from "../Spinners/DotsSpinner.vue";
 import { nextTick, ref } from "vue";
+import { useI18n } from "vue-i18n";
+
+const { t } = useI18n();
 
 const stats = {
 	wins: "N/A",
@@ -101,11 +95,11 @@ const stats = {
 	win_streak: "N/A",
 	longest_streak: "N/A",
 	most_possessed_role: {
-		display_name: "Aucun",
+		display_name: t("stats.none"),
 		image: "/assets/roles/default.png",
 	},
 	highest_win_role: {
-		display_name: "Aucun",
+		display_name: t("stats.none"),
 		image: "/assets/roles/default.png",
 	},
 };
