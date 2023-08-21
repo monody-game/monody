b6286be4b1fd38bee5c862fc25b447a9ff0f0516
moon250 <mooneupho@gmail.com>
2023-08-21 12:16:57 +0200

    feat: add couple and mayor avatar badges in chat

--- resources/js/Components/Chat/ChatMessage.vue
+++ resources/js/Components/Chat/ChatMessage.vue
@@ -10,7 +10,19 @@
 					<use href="/sprite.svg#death" />
 				</svg>
 			</span>
-			<span v-if="isWerewolf === true" class="message__is-wolf" />
+			<div class="message__badges">
+				<span v-if="isMayor === true" class="message__is-mayor">
+					<svg>
+						<use href="/sprite.svg#mayor" />
+					</svg>
+				</span>
+				<div v-if="isWerewolf === true" class="message__is-wolf" />
+				<span v-if="isCouple === true" class="message__is-couple">
+					<svg>
+						<use href="/sprite.svg#heart" />
+					</svg>
+				</span>
+			</div>
 			<img :src="avatar" alt="" />
 		</div>
 		<div class="message__texts">
@@ -39,10 +51,20 @@ const store = useStore();
 const avatar = props.message.author.avatar + "?h=50&dpr=2";
 
 const isWerewolf = ref(store.werewolves.includes(props.message.author.id));
+const isMayor = ref(store.mayor === props.message.author.id);
+const isCouple = ref(store.couple.includes(props.message.author.id));
 
 store.$subscribe((mutation, state) => {
 	if (state.werewolves.includes(props.message.author.id)) {
 		isWerewolf.value = true;
+	}
+
+	if (state.mayor === props.message.author.id) {
+		isMayor.value = true;
+	}
+
+	if (state.couple.includes(props.message.author.id)) {
+		isCouple.value = true;
 	}
 });
 </script>
