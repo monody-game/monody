e0cc2f605cc7f6f1c8f87e4d92dce3c60bdc6c97
moon250 <mooneupho@gmail.com>
2023-08-23 16:43:26 +0200

    fix: allow investigator to change his mind on who he is comparing

--- tests/Unit/Actions/InvestigatorActionTest.php
+++ tests/Unit/Actions/InvestigatorActionTest.php
@@ -8,6 +8,7 @@ use App\Enums\Role;
 use App\Facades\Redis;
 use App\Models\User;
 use App\Services\InteractionService;
+use App\Services\VoteService;
 use Tests\TestCase;
 
 class InvestigatorActionTest extends TestCase
@@ -74,6 +75,21 @@ class InvestigatorActionTest extends TestCase
         $interactionService->call(InteractionAction::Compare, $interaction['id'], $this->game['id'], 'investigator', 'parasite');
         $res = $interactionService->call(InteractionAction::Compare, $interaction['id'], $this->game['id'], 'investigator', 'sv');
         $this->assertSame($interactionService::USER_CANNOT_USE_THIS_INTERACTION, $res);
+        $interactionService->close($this->game['id'], $interaction['id']);
+    }
+
+    public function testCancellingComparisonOnOnePlayer()
+    {
+        $interactionService = app(InteractionService::class);
+        $interaction = $interactionService->create($this->game['id'], Interaction::Investigator, 'investigator');
+        $interactionService->call(InteractionAction::Compare, $interaction['id'], $this->game['id'], 'investigator', 'investigator');
+        $this->assertSame(['investigator' => ['investigator']], VoteService::getVotes($this->game['id']));
+        $interactionService->call(InteractionAction::Compare, $interaction['id'], $this->game['id'], 'investigator', 'investigator');
+        $this->assertEmpty(VoteService::getVotes($this->game['id']));
+        $res = $interactionService->call(InteractionAction::Compare, $interaction['id'], $this->game['id'], 'investigator', 'werewolf');
+        $this->assertNull($res);
+        $res = $interactionService->call(InteractionAction::Compare, $interaction['id'], $this->game['id'], 'investigator', 'sv');
+        $this->assertFalse($res);
         $interactionService->close($this->game['id'], $interaction['id']);
     }
 
